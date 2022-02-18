59f3900d35f8824a79cbadaf9f95437d5afabe05
mysteriou <elio007@hotmail.fr>
2022-02-18 06:23:37 +0100

    ajout fichier et code de base

---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+
+<html lange = "fr">
+
+<head>
+
+ </head>
+
+ <?php 
+ 
+ include dirname(__FILE__)."/php/header.php";
+ 
+ ?>
+
+<body>
+
+<div id = "main">
+
+   </div>
+
+
+   <?php 
+ 
+ include dirname(__FILE__)."/php/footer.php";
+ 
+ ?>
+
+   </body>
+
+</html>
