bf9c99ece26cd717fbea7191812f48cfb8d90440
mysteriou <elio007@hotmail.fr>
2022-02-27 13:34:59 +0100

    ajout plugin menu_header

--- php/header.php
+++ php/header.php
@@ -1,3 +1,14 @@
-<header>
-    header
+<header >
+ 
+
+<?php 
+
+$file =  dirname(__FILE__);
+    $pieces = explode("wp-content", $file);
+
+include($pieces[0].'/wp-content/plugins/menu_header/index.php');
+
+?>
+
+
 </header>
